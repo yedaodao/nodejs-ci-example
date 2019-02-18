8f1f1020a8d1836667989cd11104231f8bb6438b
yedaodao <404069912@qq.com>
2019-02-18 11:28:39 +0800

    add build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
+set -e
+
 CURRENT_PATH=`pwd`
-BUILD_LOG_FILE="/tmp/build_nodejs_ci_example.log"
 
 # 输出日志
 function print() {
     local time=now
-    local message=$1
-    echo "[INFO]${time}|${message}"
+    local prefix=$1
+    local message=${@:2}
+    # 当前时间 ex. 2019-1-1_12:00:01
+    echo "[${prefix}]`date +%Y%m%d_%H%M%S`|${message}"
+}
+
+function info() {
+    local message=$*
+    print "INFO" ${message}
+}
+
+function fatal() {
+    local message=$*
+    print "FATAL" ${message}
 }
 
 # 执行命令，将标准输入打印至日志
 function execWithLog() {
     local cmd=$@
-    print "Run Command: ${cmd}"
+    info "Run Command: ${cmd}"
     /bin/bash -c "$cmd"
-}
-
-# 当前时间 ex. 2019-1-1_12:00:01
-function now() {
-    echo `date +%Y%m%d_%H%M%S`
+    if [[ $? -ne 0 ]]
+    then
+        fatal "Run Command ERROR: $cmd"
+        exit 255
+    else
+        info "Run Command Success: $cmd"
+        return 0
+    fi
 }
 
 # 清理可能存在的脏数据
 function clear() {
+    execWithLog "rm -rf ./node_modules"
     execWithLog "rm -rf ./build"
 }
 
@@ -38,14 +55,14 @@ function build() {
 # 打包
 function package() {
     execWithLog "cp -r ./node_modules ./build"
-    execWithLog "tar -zxvf ./build -C nodejs_ci_exmaple_${now}.tar.gz"
+    execWithLog "cd ./build && tar -zcvf nodejs_ci_exmaple_`date +%Y%m%d_%H%M%S`.tar.gz *"
 }
 
 # 主函数
 function Main() {
-    print "---------- Begin build & package ----------"
+    info "---------- Begin build & package ----------"
     clear && build && package
-    print "---------- End build & package ----------"
+    info "---------- End build & package ----------"
 }
 
 Main $@
